680f9301631b52a1228b3aef8a854d95e8fe4e5e
Alexander Vasilev <alme20@yandex.ru>
2021-03-12 03:16:27 +0300

    winning results

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ Vue.use(Vuex);
 const debug = process.env.NODE_ENV !== 'production';
 
 const state = {
-  version: '0.1.4',
+  version: '0.1.5',
   isTitleShowing: true,
   showDrawer: false,
   currentTask: {}, 

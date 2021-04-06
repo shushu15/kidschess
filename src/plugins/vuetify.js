7e86bcb9d0648814c3a7c28d0140d2e028803ec5
Alexander Vasilev <alme20@yandex.ru>
2021-04-07 00:51:52 +0300

    v 0.1.10c

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 // import Vuetify from 'vuetify/lib/framework';
-// for treeshaking import from vuetify/lib
+// for treeshaking docs insist on import from vuetify/lib though result is the same as import from vuetify/lib/framework
 import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);

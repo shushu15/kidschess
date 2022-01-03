b104f93e0de5f0977820a431cc78ef1611f1643e
Alexander Vasilev <alme20@yandex.ru>
2022-01-04 00:09:50 +0300

    1.2.0d uploaded

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ Vue.use(Vuex);
 const debug = process.env.NODE_ENV !== 'production';
 
 const state = {
-  version: '1.2.0c',
+  version: '1.2.0d',
   isTitleShowing: true,
   isScreenReady: false,
   isDemo: false,
